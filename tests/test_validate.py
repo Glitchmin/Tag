db694b327044b77469a63eb83783b6ff1ecb54d2
Maks <92760953+Uiskam@users.noreply.github.com>
2022-01-24 19:27:15 +0100

    refator tests

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -12,14 +12,13 @@ class GraphTest(unittest.TestCase):
         self.assertFalse(graph1.validate(Graph(['B'], []), {0: 0}))
 
         graph2 = Graph(['A', 'B'], [(0, 1, 'ab')])
-        self.assertTrue (graph2.validate(Graph(['A'], []), {0: 0}))
+        self.assertTrue(graph2.validate(Graph(['A'], []), {0: 0}))
         self.assertFalse(graph2.validate(Graph(['A', 'B'], [(0, 1, "different")]), {0: 0, 1: 1}))
         self.assertFalse(graph2.validate(Graph(['A', 'B'], [(1, 0, "ab")]), {0: 0, 1: 1}))
         self.assertFalse(graph2.validate(Graph(['A', 'B'], []), {0: 0, 1: 1}))
-        self.assertTrue (graph2.validate(Graph(['B'], []), {0: 1}))
+        self.assertTrue(graph2.validate(Graph(['B'], []), {0: 1}))
         self.assertFalse(graph2.validate(Graph(['B'], []), {0: 0}))
         self.assertFalse(graph2.validate(Graph(['A'], []), {0: 1}))
-
 
 
 if __name__ == '__main__':
