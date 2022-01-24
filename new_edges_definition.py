6e2927aee1dbb605a3c6295d23aa4041155c79e6
Maks <92760953+Uiskam@users.noreply.github.com>
2022-01-24 23:29:45 +0100

    new edges definition accommodated to requirements

--- new_edges_definition.py
+++ new_edges_definition.py
@@ -3,10 +3,11 @@ from typing import Tuple
 
 
 class NewEdgesDefinition:
-    def __init__(self, is_outgoing: bool, label: str, rhs_vertices: List[Tuple[int, str]]):
+    def __init__(self, is_outgoing: bool, label: str, lhs_index: int, rhs_vertices: List[Tuple[int, str, bool]]):
         """for every <is_outgoing> edge with <label> removed during removing
         right graph from the start graph create edges to <rhs_vertices>
-        rhs - right hand side of the production"""
+        rhs - right hand side of the production
+        rhs_vertices[Tuple[RHS_vertex_index, new_edge_label, new_edge_direction]]"""
         self.is_outgoing = is_outgoing
         self.label = label
         self.rhs_vertices = rhs_vertices
