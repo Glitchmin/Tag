f1e90ce3ebdc8e269bb4765c178b8a2932217a8d
Maks <92760953+Uiskam@users.noreply.github.com>
2022-01-25 02:22:35 +0100

    articical merge

--- tests/test_graph_file_handling.py
+++ tests/test_graph_file_handling.py
@@ -8,9 +8,6 @@ from random_generator import RandomGraph
 
 class GraphSavingAndReadingTest(unittest.TestCase):
     def test_files_handling(self):
-        tmp = Graph(['A'], [(0, 0, 'tmp'), (0, 0, 'tmp1'), (0, 0, 'tmp2')])
-        tmp.save_to_file('tmp.csv')
-
         graphs = [Graph(['A', 'G', 'H'], [(0, 1, 'knowledge'), (1, 2, 'passion'), (2, 0, 'bond')]),
                   Graph(['A', 'G', 'H'], []),
                   Graph(['A'], [(0, 0, 'tmp'), (0, 0, 'tmp1'), (0, 0, 'tmp2')]),
@@ -19,8 +16,7 @@ class GraphSavingAndReadingTest(unittest.TestCase):
         name = "tmp.csv"
         for i in range(len(graphs)):
             graphs[i].save_to_file(name)
-            print(graphs[i].next_v_ID, Graph.read_from_file(name).next_v_ID)
-            #self.assertEqual(graphs[i], Graph.read_from_file(name))
+            self.assertEqual(graphs[i], Graph.read_from_file(name))
             os.remove(name)
 
 
