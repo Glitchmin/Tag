993a1fbe81434e4b22032cdce659d23ca77d800a
Adam <adam.p.kulczycki@gmail.com>
2022-01-26 12:26:35 +0100

    Tests for multiple files have been added

--- tests/test_production_file_handling.py
+++ tests/test_production_file_handling.py
@@ -3,13 +3,12 @@ import graph
 from graph import *
 
 
-class production_file_handling_test(unittest.TestCase):
+class ProductionFileHandlingTest(unittest.TestCase):
     def test_production_file_handling(self):
         production = Production(graph.Graph(['A'], [(0, 0, "abc")]), graph.Graph(['A'], [(0, 0, "acd")]),
                                 [NewEdgesDefinition(True, "a", 0, [("a", 0, "c", True), ("d", 1, "e", False)])])
-        production.file_counter = 0
-        production.save_to_file()
-        production2 = production.read_from_file(1)
+        file_num = production.save_to_file()
+        production2 = production.read_from_file(file_num)
 
         for i in range(len(production.new_edges_defs)):
             print(production.new_edges_defs[i].is_outgoing, production.new_edges_defs[i].label,
@@ -27,6 +26,11 @@ class production_file_handling_test(unittest.TestCase):
 
             self.assertEqual(production.new_edges_defs[i].new_edges_params,
                              production2.new_edges_defs[i].new_edges_params)
+
+    def test_multi_file_handling(self):
+        self.test_production_file_handling()
+        self.test_production_file_handling()
+        self.test_production_file_handling()
 
 
 if __name__ == '__main__':
